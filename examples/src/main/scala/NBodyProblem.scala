1f704adde017c7eec8ee8a599d73b8bc48fa53af
Benjamin Meyer <benikm91@gmail.com>
2026-08-12 15:09:56 +0200

    Clean-up n body problem

--- examples/src/main/scala/NBodyProblem.scala
+++ examples/src/main/scala/NBodyProblem.scala
@@ -1,129 +1,121 @@
 package plotwit.examples.nbody
 
 import dimwit.Conversions.given
-import dimwit._
+import dimwit.*
 import dimwit.stats.Uniform
-import io.circe.syntax._
-import plotwit._
+import io.circe.syntax.*
+import plotwit.*
 
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import scala.util.Using
 
-object NBodyProblem:
+trait Body derives Label
+trait Position derives Label
+trait Velocity derives Label
+trait Acceleration derives Label
+
+case class SystemState(
+    val masses: Tensor1[Body, Float32],
+    val positions: Tensor2[Body, Position, Float32],
+    val velocities: Tensor2[Body, Velocity, Float32]
+)
+
+case class BodyState(
+    val mass: Tensor0[Float32],
+    val position: Tensor1[Position, Float32],
+    val velocity: Tensor1[Velocity, Float32]
+)
+
+@main
+def plot(): Unit =
 
   dimwit.initialize()
 
-  trait Body derives Label
-  trait Spatial derives Label
+  // -- CONFIGURATION PARAMETERS --
 
   val numBodies = 5
   val numSpatialDimensions = 2
-
   val G = 1.0
   val dt = 0.01f
   val eps = 1e-9f
 
-  case class BodyState(mass: Tensor0[Float32], position: Tensor1[Spatial, Float32], velocity: Tensor1[Spatial, Float32]):
-    def next(acceleration: Tensor1[Spatial, Float32]): BodyState =
-      BodyState(
-        mass,
-        position + (velocity *! dt),
-        velocity + (acceleration *! dt)
-      )
+  // -- SIMULATION FUNCTIONS --
 
-  def stepFor(state: BodyState, masses: Tensor1[Body, Float32], positions: Tensor2[Body, Spatial, Float32]): BodyState =
-    // Calculate the acceleration based on the other bodies' positions and masses
-    val relativeOffsets = positions -! state.position
-    val relativeDirections = relativeOffsets /! (relativeOffsets.norm + eps)
-    val relativeSquaredDistances = relativeOffsets.pow(2).sum(Axis[Spatial]) +! eps
-    val accelerationMagnitudes = masses / relativeSquaredDistances
-    val acceleration = (relativeDirections *! accelerationMagnitudes).sum(Axis[Body]) *! G
-    // Apply the acceleration to update the body's state
-    state.next(acceleration)
+  def bodyStepFn(masses: Tensor1[Body, Float32], positions: Tensor2[Body, Position, Float32])(bodyState: BodyState): BodyState =
+    import bodyState.{mass, position, velocity}
+    val acceleration =
+      val relativeOffsets = positions -! position
+      val accelerationDirections = ((relativeOffsets /! (relativeOffsets.norm + eps)))
+        .relabel(Axis[Position] -> Axis[Acceleration])
+      val relativeSquaredDistances = relativeOffsets.pow(2).sum(Axis[Position]) +! eps
+      val accelerationMagnitudes = masses / relativeSquaredDistances
+      (accelerationDirections *! accelerationMagnitudes).sum(Axis[Body]) *! G
+    val deltaPosition = (velocity *! dt).relabelTo(Axis[Position])
+    val deltaVelocity = (acceleration *! dt).relabelTo(Axis[Velocity])
+    BodyState(
+      mass,
+      position + deltaPosition,
+      velocity + deltaVelocity
+    )
 
-  def step(masses: Tensor1[Body, Float32], positions: Tensor2[Body, Spatial, Float32], velocities: Tensor2[Body, Spatial, Float32]): (Tensor1[Body, Float32], Tensor2[Body, Spatial, Float32], Tensor2[Body, Spatial, Float32]) =
-    zipvmap(Axis[Body])(masses, positions, velocities): (m, p, v) =>
-      val state = BodyState(m, p, v)
-      val newState = stepFor(state, masses, positions)
-      (newState.mass, newState.position, newState.velocity)
-  val jitStep = jitDonatingUnsafe(step)
+  def systemStep(systemState: SystemState): SystemState =
+    val bodyStep = bodyStepFn(systemState.masses, systemState.positions)
+    val (newMasses, newPositions, newVelocities) = zipvmap(Axis[Body])((systemState.masses, systemState.positions, systemState.velocities)): (m, p, v) =>
+      val newBodyState = bodyStep(BodyState(m, p, v))
+      (newBodyState.mass, newBodyState.position, newBodyState.velocity)
+    SystemState(newMasses, newPositions, newVelocities)
+  val jitSystemStep = jitDonatingUnsafe(systemStep)
 
-  // --- Initialization (Boilerplate) ---
-  val key = Random.Key(42)
-  val (posKey, velKey, massKey) = key.splitToTuple(3)
-  val shape = Shape(Axis[Body] -> numBodies, Axis[Spatial] -> numSpatialDimensions)
-  val initialPositions = Uniform(
-    Tensor(shape).fill(-2.0f),
-    Tensor(shape).fill(2.0f)
-  ).sample(posKey)
-  val initialVelocities = Uniform(
-    Tensor(shape).fill(-0.5f),
-    Tensor(shape).fill(0.5f)
-  ).sample(velKey)
-  val masses = Uniform(
-    Tensor(Shape(Axis[Body] -> numBodies)).fill(1.0f),
-    Tensor(Shape(Axis[Body] -> numBodies)).fill(5.0f)
-  ).sample(massKey)
+  def simulate(initialState: SystemState): Iterator[SystemState] =
+    Iterator.iterate(initialState)(jitSystemStep)
 
-  // Run a few steps
-  val states: Iterator[(
-      masses: Tensor1[Body, Float32],
-      positions: Tensor2[Body, Spatial, Float32],
-      velocities: Tensor2[Body, Spatial, Float32]
-  )] =
-    Iterator.iterate((masses, initialPositions, initialVelocities))(jitStep(_, _, _))
+  // -- RUN SIMULATION AND PLOT RESULTS --
 
-  @main
-  def speed(): Unit =
-    dimwit.initialize()
-
-    // --- Initialization (Boilerplate) ---
+  // Define initial state of the system
+  val initialState =
     val key = Random.Key(42)
     val (posKey, velKey, massKey) = key.splitToTuple(3)
-    val shape = Shape(Axis[Body] -> numBodies, Axis[Spatial] -> numSpatialDimensions)
-    val initialPositions = Uniform(
-      Tensor(shape).fill(-2.0f),
-      Tensor(shape).fill(2.0f)
-    ).sample(posKey)
-    val initialVelocities = Uniform(
-      Tensor(shape).fill(-0.5f),
-      Tensor(shape).fill(0.5f)
-    ).sample(velKey)
-    val masses = Uniform(
-      Tensor(Shape(Axis[Body] -> numBodies)).fill(1.0f),
-      Tensor(Shape(Axis[Body] -> numBodies)).fill(5.0f)
-    ).sample(massKey)
+    val initialPositions =
+      val positionShape = Shape(Axis[Body] -> numBodies, Axis[Position] -> numSpatialDimensions)
+      Uniform(
+        Tensor(positionShape).fill(-2.0f),
+        Tensor(positionShape).fill(2.0f)
+      ).sample(posKey)
+    val initialVelocities =
+      val velocityShape = Shape(Axis[Body] -> numBodies, Axis[Velocity] -> numSpatialDimensions)
+      Uniform(
+        Tensor(velocityShape).fill(-0.5f),
+        Tensor(velocityShape).fill(0.5f)
+      ).sample(velKey)
+    val masses =
+      val massShape = Shape(Axis[Body] -> numBodies)
+      Uniform(
+        Tensor(massShape).fill(1.0f),
+        Tensor(massShape).fill(5.0f)
+      ).sample(massKey)
+    SystemState(masses, initialPositions, initialVelocities)
 
-    // Speed-check
-    val startTime = System.currentTimeMillis()
-    states.take(10_000).foreach(_ => ())
-    println(f"Time taken to compute 10,000 steps: ${System.currentTimeMillis() - startTime} ms")
+  // Run the simulation and collect the state trajectory to plot
+  val stateTrajectory = simulate(initialState)
+  val specTrajectory = stateTrajectory.zipWithIndex
+    .map:
+      case (state, i) =>
+        plots.scatterPlot(
+          xs = state.positions.slice(Axis[Position].at(0)),
+          ys = state.positions.slice(Axis[Position].at(1)),
+          size = state.masses,
+          _.title := f"Step $i",
+          _.encoding.x.scale.domain := List(-3, 3).asJson,
+          _.encoding.y.scale.domain := List(-3, 3).asJson
+        )
 
-  @main
-  def plot(): Unit =
-    dimwit.initialize()
+  import plotwit.PlotTargets.desktopBrowser
+  display(slider(specTrajectory.take(1200).toSeq))
 
-    val specs = states.zipWithIndex
-      .map:
-        case (state, i) =>
-          val xs = state.positions.slice(Axis[Spatial].at(0))
-          val ys = state.positions.slice(Axis[Spatial].at(1))
-          plots.scatterPlot(
-            xs,
-            ys,
-            state.masses,
-            _.title := f"Step $i",
-            _.encoding.x.scale.domain := List(-3, 3).asJson,
-            _.encoding.y.scale.domain := List(-3, 3).asJson
-          )
-
-    import plotwit.PlotTargets.desktopBrowser
-    display(slider(LazyList.from(specs).take(1200)))
-
-    // Export to Animated PNG
-    // val images = specs.map(displayAsImage(_)).take(600).toList
-    // createAnimation(images, os.pwd / "nbody_animation.gif", fps = 30)
+  // Export to Animated PNG
+  // val images = specs.map(displayAsImage(_)).take(600).toList
+  // createAnimation(images, os.pwd / "nbody_animation.gif", fps = 30)
 
 def createAnimation(images: Seq[BufferedImage], outputPath: os.Path, fps: Int): Boolean =
   val cmdString = s"ffmpeg -y -f image2pipe -vcodec png -r $fps -i - -vf split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse -loop 0"
